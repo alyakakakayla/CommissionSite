36ab815bd2e8ad77e1feb9ff815d490528d933c3
kayla <jadefoxx01@gmail.com>
2025-01-24 02:24:10 -0500

    First commit - base structure and style

---
+++ main.js
@@ -0,0 +1,32 @@
+const navWelcome = document.getElementById('welcome-item');
+const navAbout = document.getElementById('about-item');
+const navEmote = document.getElementById('emote-item');
+const navGraphic = document.getElementById('graphic-item');
+const navUI = document.getElementById('ui-item');
+const navIllus = document.getElementById('illustration-item');
+const navFanart = document.getElementById('fanart-item');
+const navConcept = document.getElementById('concept-item');
+const navContact = document.getElementById('contact-item');
+const checkbox = document.getElementById('check')
+
+navWelcome.addEventListener("click", uncheck);
+navAbout.addEventListener("click", uncheck);
+navEmote.addEventListener("click", uncheck); 
+navGraphic.addEventListener("click", uncheck);
+navUI.addEventListener("click", uncheck);
+navIllus.addEventListener("click", uncheck);
+navFanart.addEventListener("click", uncheck);
+navConcept.addEventListener("click", uncheck);
+navContact.addEventListener("click", uncheck);
+
+function uncheck() {
+    if(checkbox.checked == true) {
+        checkbox.checked = false;
+    }
+}
+
+uncheck();
+
+if(checkbox.checked == true) {
+    //hide scrollbar
+}
